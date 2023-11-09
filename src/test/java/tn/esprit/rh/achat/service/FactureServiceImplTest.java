1b7773f382a6fb6766717617ea555b7126c3e07a
haifalaabidi <92549590+haifalaabidi@users.noreply.github.com>
2023-11-09 23:55:07 +0100

    junit

--- src/test/java/tn/esprit/rh/achat/service/FactureServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/service/FactureServiceImplTest.java
@@ -10,10 +10,13 @@ import tn.esprit.rh.achat.entities.Facture;
 import tn.esprit.rh.achat.repositories.FactureRepository;
 import tn.esprit.rh.achat.services.FactureServiceImpl;
 import java.util.Date;
-
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.Set;
+import tn.esprit.rh.achat.entities.DetailFacture;
+import tn.esprit.rh.achat.entities.Fournisseur;
+import tn.esprit.rh.achat.entities.Reglement;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -28,14 +31,14 @@ class FactureServiceImplTest {
 
     @Test
     public void testGetallFactures(){
-        when(factureRepository. findAll()).thenReturn(Stream
+        when(factureRepository.findAll()).thenReturn(Stream
                 .of(new Facture(), new Facture()).collect(Collectors.toList()));
         assertEquals(2, factureService.retrieveAllFactures().size());
     }
 
     @Test
     public void saveFactureTest() {
-        Facture f = new Facture(29.75f,153.31f,new Date("19/10/2022"),new Date("21/10/2022"),true,null,null,null);
+        Facture f = new Facture(null, 29.75f, 153.31f, new Date("2022-10-19"), new Date("2022-10-21"), true, null, null, null);
         when(factureRepository.save(f)).thenReturn(f);
         assertEquals(f, factureService.addFacture(f));
     }
@@ -43,7 +46,7 @@ class FactureServiceImplTest {
     @Test
     public void retrieveFactureTest() {
         Long id = (long) 3;
-        when(factureRepository.findById(id)).thenReturn(Optional.of(new Facture(32.65f,164.84f,new Date("15/10/2022"),new Date("23/10/2022"),true,null,null,null)));
+        when(factureRepository.findById(id)).thenReturn(Optional.of(new Facture(id, 32.65f, 164.84f, new Date("2022-10-15"), new Date("2022-10-23"), true, null, null, null));
         Facture f = factureService.retrieveFacture(id);
         assertNotNull(f);
         verify(factureRepository).findById(Mockito.anyLong());
